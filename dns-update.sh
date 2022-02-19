1ed09cb2f7a7fe00c657aeba3e3aef672d63e2c3
wangheng <admin@apiof.com>
2022-02-19 18:41:44 +0800

    up

---
+++ dns-update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# author: wangheng
+# date: 2022-02-19
+
+host_content=$(/usr/bin/curl -sL https://raw.githubusercontent.com/all4fun/hosts/main/nas.txt)
+
+if [ -z "host_content" ]; then
+    echo "get remote hosts failed..."
+    exit 1
+fi
+
+echo $host_content > /etc/hosts.nas
+
+/etc/init.d/dnsmasq restart
